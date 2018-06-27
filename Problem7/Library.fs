cca22cf233d9b21e998bb8fd3923ffac40e9fee9
Saurabh Totey <SaurabhTotey@gmail.com>
2018-06-26 23:32:37 -0600

    Problem 7

---
+++ Problem7/Library.fs
@@ -0,0 +1,13 @@
+﻿namespace Problem7
+
+//Solution for Problem 7 on Project Euler
+module Problem7 =
+    open Common
+    
+    //The index of the prime number to find
+    let primeIndex = 10001
+    
+    //Generates primes until primeIndex prime numbers have been generated
+    NumberGenerator.primeSequence.generateNumbersWhile (fun currentPrimes -> currentPrimes.Count < primeIndex)
+    //Prints the last generated prime number
+    printf "%i" (NumberGenerator.primeSequence.getGeneratedNumbers().[primeIndex - 1])
