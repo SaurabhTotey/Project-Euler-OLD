f3fd116b39e0963c30ad5f7977edc6de5da623a8
Saurabh Totey <SaurabhTotey@gmail.com>
2018-08-29 21:26:32 -0600

    Really slow solution for Problem 15

---
+++ Problem15/Program.fs
@@ -0,0 +1,17 @@
+﻿module Program
+
+[<EntryPoint>]
+let main argv =
+    let gridSize = 20
+    
+    let getNumberOfPathsForGridOfSize gridSize =
+        let rec innerGetNumberOfPaths xLoc yLoc count =
+            if xLoc = gridSize && yLoc = gridSize then count + 1L
+            else if xLoc = gridSize then innerGetNumberOfPaths xLoc (yLoc + 1) count
+            else if yLoc = gridSize then innerGetNumberOfPaths (xLoc + 1) yLoc count
+            else 
+                (innerGetNumberOfPaths (xLoc + 1) yLoc count) 
+                + (innerGetNumberOfPaths xLoc (yLoc + 1) count)
+        innerGetNumberOfPaths 0 0 0L
+    printf "%i" (getNumberOfPathsForGridOfSize gridSize)
+    0
