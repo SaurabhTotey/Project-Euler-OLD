42a4ee5348ccb1d7476eb948738a6abc4b78b6db
Saurabh Totey <SaurabhTotey@gmail.com>
2018-08-31 10:13:46 -0600

    Small styling fix

--- Problem15/Program.fs
+++ Problem15/Program.fs
@@ -9,9 +9,7 @@ let main argv =
             if xLoc = gridSize && yLoc = gridSize then count + 1L
             else if xLoc = gridSize then innerGetNumberOfPaths xLoc (yLoc + 1) count
             else if yLoc = gridSize then innerGetNumberOfPaths (xLoc + 1) yLoc count
-            else 
-                (innerGetNumberOfPaths (xLoc + 1) yLoc count) 
-                + (innerGetNumberOfPaths xLoc (yLoc + 1) count)
+            else (innerGetNumberOfPaths (xLoc + 1) yLoc count) + (innerGetNumberOfPaths xLoc (yLoc + 1) count)
         innerGetNumberOfPaths 0 0 0L
     printf "%i" (getNumberOfPathsForGridOfSize gridSize)
     0
